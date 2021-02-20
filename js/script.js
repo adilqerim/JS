ce1221ef2bb8ef2628d8c4a3e715069d014c4485
Adil <example@gmail.com>
2021-02-20 18:36:31 +0300

    add cyrcle, count

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
@@ -8,20 +9,72 @@ const personalMovieDB = {
     privat: false
 };
 
-const   lastWatchedMovie = prompt('Один из последних просмотренных фильмов?', ''),
-        rateLastWatchedMovie = prompt('На сколько оцените его?', ''),
-        lastWatchedMovie2 = prompt('Один из последних просмотренных фильмов?', ''),
-        rateLastWatchedMovie2 = prompt('На сколько оцените его?', '');
 
-// personalMovieDB.movies = {
-//     lastWatchedMovie: rateLastWatchedMovie,
-//     lastWatchedMovie2: rateLastWatchedMovie2
-// };
+for (let i = 0; i < 2; i++) {
 
-// personalMovieDB.movies.lastWatchedMovie = rateLastWatchedMovie;
-// personalMovieDB.movies.lastWatchedMovie2 = rateLastWatchedMovie2;
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+    
+    // if (a === '' || a === null || a.length > 50){
+    //     i--;
+    // }   else if (b === '' || b === null || b.length > 50){
+    //     i--;
+    // }   else {
+    //         personalMovieDB.movies[a] = b;
+    // } 
 
-personalMovieDB.movies[lastWatchedMovie] = rateLastWatchedMovie;
-personalMovieDB.movies[lastWatchedMovie2] = rateLastWatchedMovie2;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+// let i = 0;
+// while(i < 2) {
+
+//     const a = prompt('Один из последних просмотренных фильмов?', '');
+//     const b = prompt('На сколько оцените его?', '');
+
+//     if (a === '' || a === null || a.length > 50){
+//         i = 0;
+//     }   else if (b === '' || b === null || b.length > 50){
+//         i = 0;
+//     }   else {
+//         personalMovieDB.movies[a] = b;
+//         i++;
+//     }
+// }
+
+
+// let i = 0;
+// do {
+
+//     const a = prompt('Один из последних просмотренных фильмов?', '');
+//     const b = prompt('На сколько оцените его?', '');
+
+//     if (a === '' || a === null || a.length > 50){
+//         i = 0;
+//     }   else if (b === '' || b === null || b.length > 50){
+//         i = 0;
+//     }   else {
+//         personalMovieDB.movies[a] = b;
+//         i++;
+//     }
+// }
+// while( i < 2);
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов.');
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+    alert('Вы классический зритель');
+} else if(personalMovieDB.count >= 30){
+    alert('Вы киноман!');
+} else {
+    alert('Произошла ошибка');
+}
+
 
 console.log(personalMovieDB);
